b0c1206fd52b0e1143fc57e08a3bf5573e7b2ddc
himalayanZephyr <sumeetdhariwal@gmail.com>
2023-07-18 13:59:32 +0100

    adding solution

---
+++ Week6/Longest_Palindromic_Substring/Solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+public:
+
+    std::pair<int, int> get_palindrome(int l, int r, std::string s) {
+        while(l >= 0 and r < s.size() and (s[l] == s[r])) {
+            l--;
+            r++;
+        }
+        return {l+1, r-1};
+    }
+
+    string longestPalindrome(string s) {
+        
+        int L = 0;
+        int R = 0;
+
+        for(int i=0; i<s.size(); i++){
+
+            auto [l, r] = get_palindrome(i,i,s);
+            if((r-l) > (R-L)){
+                L = l;
+                R = r;
+            }
+            
+            std::tie(l, r) = get_palindrome(i,i+1,s);
+            if((r-l) > (R-L)){
+                L = l;
+                R = r;
+            }            
+        }
+
+        return s.substr(L, R-L+1);
+
+    }
+};
