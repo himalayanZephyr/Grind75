60a1ed2a216961544e93609a137052197d52e93c
himalayanZephyr <sumeetdhariwal@gmail.com>
2023-08-08 17:10:47 +0100

    linked list cycle

---
+++ src/Week1/Linked_List_Cycle/Solution.cpp
@@ -0,0 +1,25 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (slow and fast and fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
